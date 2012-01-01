f8b32f5c5fa23424ee1fc1f4fa9397f00684a61a
Jean-Louis GUENEGO <jlguenego@gmail.com>
2012-01-01 13:20:43 +0100

    add open directory button

--- prototypes/v1/OCP/src/com/guenego/ocp/gui/console/UserComposite.java
+++ prototypes/v1/OCP/src/com/guenego/ocp/gui/console/UserComposite.java
@@ -1,8 +1,11 @@
 package com.guenego.ocp.gui.console;
 
+import java.io.File;
+
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.program.Program;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.DirectoryDialog;
@@ -59,6 +62,20 @@ public class UserComposite extends Composite {
 		btnBrowse.setBounds(283, 61, 68, 23);
 		btnBrowse.setText("Browse");
 
+		Button btnOpen = new Button(this, SWT.NONE);
+		btnOpen.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				File file = new File(dirText.getText());
+				if (file.isDirectory()) {
+					Program.launch(dirText.getText());
+				}
+			}
+		});
+		btnOpen.setBounds(357, 61, 68, 23);
+		btnOpen.setText("Open");
+
+		
 		Button checkoutButton = new Button(this, SWT.NONE);
 		checkoutButton.addSelectionListener(new SelectionAdapter() {
 			@Override
@@ -89,6 +106,7 @@ public class UserComposite extends Composite {
 		});
 		commitButton.setBounds(209, 88, 68, 23);
 		commitButton.setText("Commit");
+		
 
 	}
 }
