807d11191fe9e320e9431150cd07ad969ae442be
Jean-Louis GUENEGO <jlguenego@gmail.com>
2012-01-06 14:47:03 +0100

    explorer: adding up function

--- prototypes/v1/OCP/src/com/guenego/ocp/gui/console/UserExplorerComposite.java
+++ prototypes/v1/OCP/src/com/guenego/ocp/gui/console/UserExplorerComposite.java
@@ -18,20 +18,25 @@ import org.eclipse.swt.widgets.TableColumn;
 import org.eclipse.swt.widgets.TableItem;
 import org.eclipse.wb.swt.SWTResourceManager;
 
+import com.guenego.misc.JLG;
 import com.guenego.ocp.Agent;
 import com.guenego.ocp.Pointer;
 import com.guenego.ocp.Tree;
 import com.guenego.ocp.TreeEntry;
 import com.guenego.ocp.User;
+import org.eclipse.swt.events.MouseAdapter;
+import org.eclipse.swt.events.MouseEvent;
 
 public class UserExplorerComposite extends Composite {
 	private static final String DIRECTORY_SIZE = "";
 	private static final String DIRECTORY_TYPE = "Directory";
+	private static final String DIRECTORY_PARENT = "..";
 	private static final String FILE_TYPE = "File";
 	private static final Image DIRECTORY_ICON = SWTResourceManager.getImage(
 			UserExplorerComposite.class, "directory.png");
 	private static final Image FILE_ICON = SWTResourceManager.getImage(
 			UserExplorerComposite.class, "file.png");
+
 	private Table localDirectoryTable;
 	private Table remoteDirectoryTable;
 	private File currentLocalDirectory;
@@ -40,6 +45,7 @@ public class UserExplorerComposite extends Composite {
 
 	private Agent agent;
 	private User user;
+	private Label localDirectoryLabel;
 
 	/**
 	 * Create the composite.
@@ -74,14 +80,34 @@ public class UserExplorerComposite extends Composite {
 		gl_leftComposite.horizontalSpacing = 0;
 		leftComposite.setLayout(gl_leftComposite);
 
-		Label localDirectoryLabel = new Label(leftComposite, SWT.NONE);
+		localDirectoryLabel = new Label(leftComposite, SWT.NONE);
 		localDirectoryLabel.setToolTipText("Local Directory");
 		localDirectoryLabel.setLayoutData(new GridData(SWT.FILL, SWT.FILL,
 				true, false, 1, 1));
-		localDirectoryLabel.setText(currentLocalDirectory.getAbsolutePath());
 
 		localDirectoryTable = new Table(leftComposite, SWT.BORDER
 				| SWT.FULL_SELECTION);
+		localDirectoryTable.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseDoubleClick(MouseEvent e) {
+				JLG.debug("double click");
+				TableItem[] items = localDirectoryTable.getSelection();
+				if (items.length == 1) {
+					TableItem item = items[0];
+					String type = item.getText(1);
+					JLG.debug("type = " + type);
+					if (type.equals(DIRECTORY_TYPE)) {
+						String name = item.getText(0);
+						if (name.equals(DIRECTORY_PARENT)) {
+							currentLocalDirectory = currentLocalDirectory
+									.getParentFile();
+							reloadLocalDirectoryTable();
+						}
+					}
+				}
+				JLG.debug("table item:" + e.widget.getClass());
+			}
+		});
 		GridData gd_localDirectoryTable = new GridData(SWT.FILL, SWT.FILL,
 				true, true, 1, 1);
 		gd_localDirectoryTable.heightHint = 275;
@@ -103,44 +129,7 @@ public class UserExplorerComposite extends Composite {
 		localSizeColumn.setWidth(100);
 		localSizeColumn.setText("Size");
 
-		TableItem parentDirtableItem = new TableItem(localDirectoryTable,
-				SWT.NONE);
-		parentDirtableItem.setText(new String[] { "..", DIRECTORY_TYPE,
-				DIRECTORY_SIZE });
-		parentDirtableItem.setImage(SWTResourceManager.getImage(
-				UserExplorerComposite.class, "directory.png"));
-
-		File[] children = currentLocalDirectory.listFiles();
-		
-		Arrays.sort(children, new Comparator<File>() {
-			public int compare(File f1, File f2) {
-				if (f1.isFile() && f2.isDirectory()) {
-					return 1;
-				} else if (f2.isFile() && f1.isDirectory()) {
-					return -1;
-				} else {
-					return f1.getName().compareTo(f2.getName());
-				}
-			}
-		});
-
-		for (File f : children) {
-			TableItem tableItem = new TableItem(localDirectoryTable, SWT.NONE);
-			String type = null;
-			String size = null;
-			Image image = null;
-			if (f.isDirectory()) {
-				type = DIRECTORY_TYPE;
-				size = "";
-				image = DIRECTORY_ICON;
-			} else {
-				type = FILE_TYPE;
-				size = (f.length() / 1024) + " KB";
-				image = FILE_ICON;
-			}
-			tableItem.setText(new String[] { f.getName(), type, size });
-			tableItem.setImage(image);
-		}
+		reloadLocalDirectoryTable();
 
 		Composite rightComposite = new Composite(sashForm, SWT.NONE);
 		GridLayout gl_rightComposite = new GridLayout(1, false);
@@ -176,8 +165,8 @@ public class UserExplorerComposite extends Composite {
 
 		TableItem parentTreetableItem = new TableItem(remoteDirectoryTable,
 				SWT.NONE);
-		parentTreetableItem.setText(new String[] { "..", DIRECTORY_TYPE,
-				DIRECTORY_SIZE });
+		parentTreetableItem.setText(new String[] { DIRECTORY_PARENT,
+				DIRECTORY_TYPE, DIRECTORY_SIZE });
 		parentTreetableItem.setImage(SWTResourceManager.getImage(
 				UserExplorerComposite.class, "directory.png"));
 
@@ -220,6 +209,70 @@ public class UserExplorerComposite extends Composite {
 		}
 
 		sashForm.setWeights(new int[] { 1, 1 });
+
+	}
+
+	protected void reloadLocalDirectoryTable() {
+		String path = "";
+		if (currentLocalDirectory != null) {
+			path = currentLocalDirectory.getAbsolutePath();
+		}
+		localDirectoryLabel.setText(path);
+		// first make sure the table content is empty.
+		localDirectoryTable.removeAll();
+
+		if (currentLocalDirectory != null) {
+			TableItem parentDirtableItem = new TableItem(localDirectoryTable,
+					SWT.NONE);
+			parentDirtableItem.setText(new String[] { DIRECTORY_PARENT,
+					DIRECTORY_TYPE, DIRECTORY_SIZE });
+			parentDirtableItem.setImage(SWTResourceManager.getImage(
+					UserExplorerComposite.class, "directory.png"));
+		}
+
+		File[] children = null;
+		if (currentLocalDirectory != null) {
+			children = currentLocalDirectory.listFiles();
+		} else {
+			children = File.listRoots();
+		}
+
+		Arrays.sort(children, new Comparator<File>() {
+			public int compare(File f1, File f2) {
+				if (f1.isFile() && f2.isDirectory()) {
+					return 1;
+				} else if (f2.isFile() && f1.isDirectory()) {
+					return -1;
+				} else {
+					return f1.getName().compareTo(f2.getName());
+				}
+			}
+		});
+
+		for (File f : children) {
+			JLG.debug("filename: " + f.getAbsolutePath());
+			TableItem tableItem = new TableItem(localDirectoryTable, SWT.NONE);
+			String type = null;
+			String size = null;
+			Image image = null;
+			if (f.isDirectory()) {
+				type = DIRECTORY_TYPE;
+				size = "";
+				image = DIRECTORY_ICON;
+			} else {
+				type = FILE_TYPE;
+				size = (f.length() / 1024) + " KB";
+				image = FILE_ICON;
+			}
+			String name = null;
+			if (currentLocalDirectory == null) {
+				name = f.getAbsolutePath();
+			} else {
+				name = f.getName();
+			}
+			tableItem.setText(new String[] { name, type, size });
+			tableItem.setImage(image);
+		}
 
 	}
 
