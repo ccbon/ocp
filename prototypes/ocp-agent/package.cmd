c4a49d1e67ad3c6b521ecec1e6e00f57a3bf09a4
Ejder Bastug <ejder.bastug@gmail.com>
2012-02-12 00:53:15 +0100

    - Target folder has been excluded from project structure (It's useless to commit temporary files) - New profiles has been added to POM: Linux, Macosx - Assembly XML files has been created for each profile - Batch script has been created to trigger all profiles. You can type "package" from console in order to package all jar files for different platforms.

---
+++ prototypes/ocp-agent/package.cmd
@@ -0,0 +1,6 @@
+call mvn package -P windows-x86_64
+call mvn package -P windows-x86
+call mvn package -P linux-x86_64
+call mvn package -P linux-x86
+call mvn package -P macosx-cacao-x86_64
+call mvn package -P macosx-cacao-x86
