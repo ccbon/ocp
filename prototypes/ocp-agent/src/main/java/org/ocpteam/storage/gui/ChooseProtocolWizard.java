c15495ac5cc4c34b137471aa5fa8b521515461aa
Jean-Louis GUENEGO <jlguenego@gmail.com>
2012-02-15 10:26:03 +0100

    adding protocol choice for testing.

---
+++ prototypes/ocp-agent/src/main/java/org/ocpteam/storage/gui/ChooseProtocolWizard.java
@@ -0,0 +1,34 @@
+package org.ocpteam.storage.gui;
+
+import java.util.Map;
+import java.util.Properties;
+
+import org.eclipse.jface.wizard.Wizard;
+import org.ocpteam.misc.JLG;
+import org.ocpteam.ocp.OCPAgent;
+
+public class ChooseProtocolWizard extends Wizard {
+
+	private ChooseProtocolWizardPage p1;
+	private Map<String, String> map;
+
+	public ChooseProtocolWizard(Map<String, String> map) {
+		setWindowTitle("New Wizard");
+		this.map = map;
+	}
+
+	@Override
+	public void addPages() {
+		p1 = new ChooseProtocolWizardPage(map);
+		addPage(p1);
+	}
+
+	@Override
+	public boolean performFinish() {
+		Properties p = new Properties();
+		p.setProperty("agent.class", p1.selection);
+		JLG.storeConfig(p, "protocol.properties");
+		return true;
+	}
+
+}
---
+++ prototypes/ocp-agent/src/main/java/org/ocpteam/storage/gui/ChooseProtocolWizardPage.java
@@ -0,0 +1,59 @@
+package org.ocpteam.storage.gui;
+
+import java.util.Map;
+
+import org.eclipse.jface.wizard.WizardPage;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Combo;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.ocpteam.misc.JLG;
+
+public class ChooseProtocolWizardPage extends WizardPage {
+
+	public Combo combo;
+	private Map<String, String> map;
+	public String selection;
+	
+	/**
+	 * Create the wizard.
+	 */
+	public ChooseProtocolWizardPage(Map<String, String> map) {
+		super("wizardPage");
+		setTitle("Choose a protocol to test");
+		setDescription("Please choose the protocol you wish to use.");
+		this.map = map;
+	}
+
+	/**
+	 * Create contents of the wizard.
+	 * @param parent
+	 */
+	public void createControl(Composite parent) {
+		Composite container = new Composite(parent, SWT.NULL);
+
+		setControl(container);
+		
+		Label lblProtocol = new Label(container, SWT.NONE);
+		lblProtocol.setBounds(86, 101, 157, 13);
+		lblProtocol.setText("Protocol");
+		
+		final String[] keys = (String[]) map.keySet().toArray(new String[map.size()]);
+		
+		combo = new Combo(container, SWT.READ_ONLY);
+		combo.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				selection = map.get(keys[combo.getSelectionIndex()]);
+				JLG.debug("selection = " + selection);
+			}
+		});
+		
+		combo.setItems(keys);
+		combo.setBounds(84, 120, 159, 21);
+		combo.select(0);
+		selection = map.get(keys[0]);
+	}
+}
