b7686438d9bc8125e537f2e5dabdd87aa320a760
Jean-Louis GUENEGO <jlguenego@gmail.com>
2012-03-12 15:11:01 +0100

    adding a simple junit test.

---
+++ prototypes/ocp-agent/src/test/java/org/ocpteam/ocp_agent/OCPSimpleTest.java
@@ -0,0 +1,41 @@
+package org.ocpteam.ocp_agent;
+
+import static org.junit.Assert.*;
+
+import org.ocpteam.core.Application;
+import org.ocpteam.functionality.DataSourceFactory;
+import org.ocpteam.functionality.TestScenario;
+import org.ocpteam.protocol.ocp.OCPDataSource;
+
+public class OCPSimpleTest extends TestScenario {
+
+	@org.junit.Test
+	public void simple() {
+		try {
+			Application app = new Application();
+			app.designer.add(DataSourceFactory.class);
+			DataSourceFactory dsf = app.designer.get(DataSourceFactory.class);
+			dsf.designer.add(OCPDataSource.class);
+			app.designer.add(TestScenario.class, new OCPSimpleTest());
+
+			assertTrue(app.designer.get(TestScenario.class).test());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public boolean test() {
+		try {
+			System.out.println("Hello Test Scenario");
+			DataSourceFactory dsf = app.designer.get(DataSourceFactory.class);
+			OCPDataSource ds = (OCPDataSource) dsf.getInstance("OCP");
+			ds.open();
+			ds.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		}
+		return true;
+	}
+}
