16fe3340073779d782e1bec977f4e4d9167fd3d4
Jean-Louis GUENEGO <jlguenego@gmail.com>
2012-03-13 11:16:00 +0100

    improving test isolation example... You don't need to use anymore application object to test a datasource.

--- prototypes/ocp-agent/src/test/java/org/ocpteam/ocp_agent/OCPSimpleTest.java
+++ prototypes/ocp-agent/src/test/java/org/ocpteam/ocp_agent/OCPSimpleTest.java
@@ -2,9 +2,8 @@ package org.ocpteam.ocp_agent;
 
 import static org.junit.Assert.assertTrue;
 
-import org.ocpteam.core.Application;
-import org.ocpteam.functionality.DataSourceFactory;
 import org.ocpteam.functionality.TestScenario;
+import org.ocpteam.layer.rsp.DataSource;
 import org.ocpteam.protocol.ocp.OCPDataSource;
 
 public class OCPSimpleTest extends TestScenario {
@@ -12,13 +11,7 @@ public class OCPSimpleTest extends TestScenario {
 	@org.junit.Test
 	public void simple() {
 		try {
-			Application app = new Application();
-			app.getDesigner().add(DataSourceFactory.class);
-			DataSourceFactory dsf = app.getDesigner().get(DataSourceFactory.class);
-			dsf.getDesigner().add(OCPDataSource.class);
-			app.getDesigner().add(TestScenario.class, new OCPSimpleTest());
-
-			assertTrue(app.getDesigner().get(TestScenario.class).test());
+			assertTrue(new OCPSimpleTest().test());
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -28,8 +21,7 @@ public class OCPSimpleTest extends TestScenario {
 	public boolean test() {
 		try {
 			System.out.println("Hello Test Scenario");
-			DataSourceFactory dsf = parent.getDesigner().get(DataSourceFactory.class);
-			OCPDataSource ds = (OCPDataSource) dsf.getInstance("OCP");
+			DataSource ds = new OCPDataSource();
 			ds.open();
 			ds.close();
 		} catch (Exception e) {
