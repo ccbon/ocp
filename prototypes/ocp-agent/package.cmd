a92a8df90a0fccbb03111fc69618556e5d3b7a1f
THOMIAS Yannis <thomias.yannis@gmail.com>
2013-01-17 12:59:08 +0100

    ...

--- prototypes/ocp-agent/package.cmd
+++ prototypes/ocp-agent/package.cmd
@@ -16,12 +16,12 @@ call mvn package -P macosx-cacao-x86_64 -DskipTests
 call mvn package -P macosx-cacao-x86 -DskipTests
 
 setlocal
-cp build/gdse_64.l4j "%LAUNCH4J_HOME%\gdse_64.l4j"
-cp build/gdse_32.l4j "%LAUNCH4J_HOME%\gdse_32.l4j"
-cp target/ocp-agent-complete-windows-x86_64.jar "%LAUNCH4J_HOME%\ocp-agent-complete-windows-x86_64.jar"
-cp target/ocp-agent-complete-windows-x86_32.jar "%LAUNCH4J_HOME%\ocp-agent-complete-windows-x86_32.jar"
-cp build/Soccer.ico "%LAUNCH4J_HOME%\Soccer.ico"
-cd "%LAUNCH4J_HOME%"
+copy build/gdse_64.l4j "%LAUNCH4J_HOME%\gdse_64.l4j"
+copy build/gdse_32.l4j "%LAUNCH4J_HOME%\gdse_32.l4j"
+copy target/ocp-agent-complete-windows-x86_64.jar "%LAUNCH4J_HOME%\ocp-agent-complete-windows-x86_64.jar"
+copy target/ocp-agent-complete-windows-x86_32.jar "%LAUNCH4J_HOME%\ocp-agent-complete-windows-x86_32.jar"
+copy build/Soccer.ico "%LAUNCH4J_HOME%\Soccer.ico"
+copy "%LAUNCH4J_HOME%"
 launch4jc gdse_64.l4j
 launch4jc gdse_32.l4j
 endlocal
@@ -30,10 +30,10 @@ move /Y "%LAUNCH4J_HOME%\gdse_x32.exe" "target\gdse_x32.exe"
 
 setlocal
 mkdir target\nsis
-cp target\gdse_x64.exe target\nsis\gdse_x64.exe
-cp target\gdse_x32.exe target\nsis\gdse_x32.exe
-cp build\Soccer.ico target\nsis\Soccer.ico
-cp build\gdse.nsi target\nsis\gdse.nsi
+copy target\gdse_x64.exe target\nsis\gdse_x64.exe
+copy target\gdse_x32.exe target\nsis\gdse_x32.exe
+copy build\Soccer.ico target\nsis\Soccer.ico
+copy build\gdse.nsi target\nsis\gdse.nsi
 cd target\nsis
 "%NSIS_HOME%\makensis.exe" gdse.nsi
 endlocal
